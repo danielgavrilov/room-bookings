aaef7ea16389665b69612c70bb341eb0c7e411ba
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-07-03 22:09:37 +0100

    Implement requesting & caching bookings

--- src/api/index.js
+++ src/api/index.js
@@ -1,29 +1,69 @@
+import R from 'ramda';
 import moment from 'moment';
 
-const cache = {
-  "2017-03-21": {
-    requested: "2017-07-03T16:47:00.705Z",
-    rooms: {
-      "{siteid}-{roomid}-{class}": [
-        {
-            "slotid": 991362,
-            "start_time": "2017-07-03T09:00:00+01:00",
-            "end_time": "2017-07-03T17:00:00+01:00",
-            "description": "UCL Contracted Rooms",
-            "phone": null,
-            "weeknumber": 45.0,
-            "contact": "Room Closed"
-        }
-      ]
-    }
-  }
-}
+import { getBookings } from './endpoints';
+import { getDateKey, getUniqueRoomKey } from '../utils/keys';
+
+import todayBookings from '../data/bookings.json';
+
+const CACHE_TODAY_MINUTES = 2;
+const CACHE_DEFAULT_MINUTES = 10;
 
 const cache = {};
 
-// startDate & endDate are both inclusive
-export function getAllBookings({ startDate, endDate }) {
-  const start = moment(startDate).startOf("day").toDate();
-  const end = moment(endDate).endOf("day").toDate();
+function expired(dateKey) {
+  if (cache[dateKey] == null) return true;
+  const minutes = getDateKey(new Date()) === dateKey ?
+                  CACHE_TODAY_MINUTES :
+                  CACHE_DEFAULT_MINUTES;
+  return (new Date() - cache[dateKey].requested) / 1000 / 60 > minutes;
+}
 
+function requestBookingsForDay(date) {
+  const start = moment(date).startOf("day").toDate();
+  const end = moment(date).endOf("day").toDate();
+  return getBookings({ start, end });
+}
+
+function structureBookings(bookings) {
+  // TODO remove duplication of roomname roomid?
+  return R.groupBy(getUniqueRoomKey, bookings);
+}
+
+function cacheBookings(dateKey, bookingsByRoom) {
+  const cacheItem = cache[dateKey];
+  cacheItem.pending = false;
+  cacheItem.bookingsByRoom = bookingsByRoom;
+  return bookingsByRoom;
+}
+
+export function getBookingsForDay(date) {
+
+  return new Promise((resolve) => resolve(todayBookings));
+
+  // TODO uncomment
+
+  // const dateKey = getDateKey(date);
+  //
+  // if (cache[dateKey] && cache[dateKey].pending) {
+  //   return cache[dateKey].promise;
+  // } else if (cache[dateKey] && !cache[dateKey].pending && !expired(dateKey)) {
+  //   return new Promise((resolve) => resolve(cache[dateKey].bookingsByRoom));
+  // } else {
+  //
+  //   const promise = requestBookingsForDay(date)
+  //     .then(structureBookings)
+  //     .then((bookingsByRoom) => {
+  //        return cacheBookings(dateKey, bookingsByRoom);
+  //      });
+  //
+  //   cache[dateKey] = {
+  //     date: dateKey,
+  //     pending: true,
+  //     requested: new Date(),
+  //     promise
+  //   };
+  //
+  //   return promise;
+  // }
 }
--- src/utils/keys.js
+++ src/utils/keys.js
@@ -1,3 +1,9 @@
+import moment from 'moment';
+
+export function getDateKey(date) {
+  return moment(date).format("YYYY-MM-DD");
+}
+
 export function getUniqueRoomKey({ roomid, siteid }) {
   return siteid + "-" + roomid;
 }
