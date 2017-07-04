8a09ac52a24f95b9e87c26fa7866008df977cab4
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-07-04 11:54:59 +0100

    Install redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -9,8 +10,12 @@ import rootReducer from './reducers';
 
 import './index.css';
 
-const state = {};
-const store = createStore(rootReducer, state);
+const initialState = {};
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(thunk)
+);
 
 ReactDOM.render(
   <Provider store={store}>
