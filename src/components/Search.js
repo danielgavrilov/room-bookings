c24d79671cb78796d29b2bcb7a32fee921e7cc85
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-07-05 01:20:28 +0100

    Add date picker

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,62 +1,113 @@
 import React from 'react';
+import moment from '../moment';
+import { connect } from 'react-redux'
+import DayPicker from 'react-day-picker';
 
+import 'react-day-picker/lib/style.css';
 import './Search.css';
 
-const Search = () => (
-  <form id="search" className="search-form">
-    I need a room for
-    <select name="hours" id="hours" defaultValue="...">
-      <option value="...">...</option>
-      <option value="0:30">30min</option>
-      <option value="1:00">1h</option>
-      <option value="1:30">1h 30min</option>
-      <option value="2:00">2h</option>
-      <option value="2:30">2h 30min</option>
-      <option value="3:00">3h</option>
-      <option value="3:30">3h 30min</option>
-      <option value="4:00">4h</option>
-      <option value="4:30">4h 30min</option>
-      <option value="5:00">5h</option>
-      <option value="5:30">5h 30min</option>
-      <option value="6:00">6h</option>
-      <option value="6:30">6h 30min</option>
-      <option value="7:00">7h</option>
-    </select>
-    between
-    <select name="start" id="start" defaultValue="08:00">
-      <option value="08:00">08:00</option>
-      <option value="08:30">08:30</option>
-      <option value="09:00">09:00</option>
-      <option value="09:30">09:30</option>
-      <option value="10:00">10:00</option>
-      <option value="10:30">10:30</option>
-      <option value="11:00">11:00</option>
-      <option value="11:30">11:30</option>
-      <option value="12:00">12:00</option>
-      <option value="12:30">12:30</option>
-      <option value="13:00">13:00</option>
-      <option value="13:30">13:30</option>
-      <option value="14:00">14:00</option>
-      <option value="14:30">14:30</option>
-    </select>
-    and
-    <select name="end" id="end" defaultValue="14:00">
-      <option value="08:00">08:00</option>
-      <option value="08:30">08:30</option>
-      <option value="09:00">09:00</option>
-      <option value="09:30">09:30</option>
-      <option value="10:00">10:00</option>
-      <option value="10:30">10:30</option>
-      <option value="11:00">11:00</option>
-      <option value="11:30">11:30</option>
-      <option value="12:00">12:00</option>
-      <option value="12:30">12:30</option>
-      <option value="13:00">13:00</option>
-      <option value="13:30">13:30</option>
-      <option value="14:00">14:00</option>
-      <option value="14:30">14:30</option>
-    </select>
-  </form>
+import {
+  searchDate,
+  searchHours,
+  searchBetween,
+  searchCapacity,
+  fetchBookings
+} from '../actions';
+
+const Search = ({
+  active,
+  date,
+  between,
+  selectDate
+}) => (
+  <div className="search-form">
+    <div className="textual">
+      <form id="search" className="search-form">
+        I need a room for
+        <select name="hours" id="hours" defaultValue="...">
+          <option value="...">...</option>
+          <option value="00:30">30min</option>
+          <option value="01:00">1h</option>
+          <option value="01:30">1h 30min</option>
+          <option value="02:00">2h</option>
+          <option value="02:30">2h 30min</option>
+          <option value="03:00">3h</option>
+          <option value="03:30">3h 30min</option>
+          <option value="04:00">4h</option>
+          <option value="04:30">4h 30min</option>
+          <option value="05:00">5h</option>
+          <option value="05:30">5h 30min</option>
+          <option value="06:00">6h</option>
+          <option value="06:30">6h 30min</option>
+          <option value="07:00">7h</option>
+        </select>
+        between
+        <select name="start" id="start" defaultValue="08:00">
+          <option value="08:00">08:00</option>
+          <option value="08:30">08:30</option>
+          <option value="09:00">09:00</option>
+          <option value="09:30">09:30</option>
+          <option value="10:00">10:00</option>
+          <option value="10:30">10:30</option>
+          <option value="11:00">11:00</option>
+          <option value="11:30">11:30</option>
+          <option value="12:00">12:00</option>
+          <option value="12:30">12:30</option>
+          <option value="13:00">13:00</option>
+          <option value="13:30">13:30</option>
+          <option value="14:00">14:00</option>
+          <option value="14:30">14:30</option>
+        </select>
+        and
+        <select name="end" id="end" defaultValue="14:00">
+          <option value="08:00">08:00</option>
+          <option value="08:30">08:30</option>
+          <option value="09:00">09:00</option>
+          <option value="09:30">09:30</option>
+          <option value="10:00">10:00</option>
+          <option value="10:30">10:30</option>
+          <option value="11:00">11:00</option>
+          <option value="11:30">11:30</option>
+          <option value="12:00">12:00</option>
+          <option value="12:30">12:30</option>
+          <option value="13:00">13:00</option>
+          <option value="13:30">13:30</option>
+          <option value="14:00">14:00</option>
+          <option value="14:30">14:30</option>
+        </select>
+      </form>
+    </div>
+
+    <div className="calendar">
+      <DayPicker
+        onDayClick={selectDate}
+        selectedDays={[date.toDate()]}
+        enableOutsideDays
+      />
+    </div>
+  </div>
 )
 
-export default Search;
+const mapStateToProps = (state, ownProps) => {
+  const { active, date, between } = state;
+  return {
+    active,
+    date,
+    between
+  };
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    selectDate: (date) => {
+      date = moment(date).startOf("day");
+      dispatch(searchDate(date));
+      dispatch(fetchBookings(date));
+    }
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
