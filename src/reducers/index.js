1f496a3eab768589b2562625b960f701092e3df6
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-07-05 01:10:55 +0100

    Implement reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,73 @@
-export default function(state) {
-  // TODO
+import {
+  FETCH_BOOKINGS_REQUEST,
+  FETCH_BOOKINGS_SUCCESS,
+  FETCH_BOOKINGS_FAILURE,
+  SEARCH_DATE,
+  SEARCH_HOURS,
+  SEARCH_BETWEEN,
+  SEARCH_CAPACITY,
+} from '../actions';
+
+export default function(state, action) {
+  switch (action.type) {
+
+    case FETCH_BOOKINGS_REQUEST:
+      if (action.date.isSame(state.date, "day")) {
+        return {
+          ...state,
+          loading: true,
+          error: null
+        }
+      }
+      return state;
+
+    case FETCH_BOOKINGS_SUCCESS:
+      if (action.date.isSame(state.date, "day")) {
+        console.log(action.bookingsByRoom);
+        return {
+          ...state,
+          loading: false,
+          bookingsByRoom: action.bookingsByRoom
+        }
+      }
+      return state;
+
+    case FETCH_BOOKINGS_FAILURE:
+      if (action.date.isSame(state.date, "day")) {
+        return {
+          ...state,
+          loading: false,
+          error: action.error
+        }
+      }
+      return state;
+
+    case SEARCH_DATE:
+      return {
+        ...state,
+        date: action.date
+      }
+
+    case SEARCH_HOURS:
+      return {
+        ...state,
+        hours: action.hours
+      }
+
+    case SEARCH_BETWEEN:
+      return {
+        ...state,
+        between: action.between
+      }
+
+    case SEARCH_CAPACITY:
+      return {
+        ...state,
+        capacity: action.capacity
+      }
+
+    default:
+      return state;
+  }
   return state;
 }
